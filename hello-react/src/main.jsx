f3b46c73f81e11400174be3d35b3725fe591e579
Soe Yi Yi Aung <syya133@gmail.com>
2025-12-18 15:42:39 +0630

    update

--- hello-react/src/main.jsx
+++ hello-react/src/main.jsx
@@ -1,17 +1,47 @@
+// main.jsx
+
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
+
 import AppProvider from "./AppProvider.jsx";
 
+ 
+
 import "@fontsource/roboto/300.css";
+
 import "@fontsource/roboto/400.css";
+
 import "@fontsource/roboto/500.css";
+
 import "@fontsource/roboto/700.css";
 
+ 
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+ 
+
+const queryClient = new QueryClient();
+
+ 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<AppProvider>
-			<App />
-		</AppProvider>
-	</React.StrictMode>
+
+    <React.StrictMode>
+
+        <QueryClientProvider client={queryClient}>
+
+            <AppProvider>
+
+                <App />
+
+            </AppProvider>
+
+        </QueryClientProvider>
+
+    </React.StrictMode>
+
 );

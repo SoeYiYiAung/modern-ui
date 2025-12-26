0e47f0b7763bded53ea8023f64e760caf3a907f3
Soe Yi Yi Aung - Nexstack <yiyi@nexstack.sg>
2025-12-26 21:50:38 +0630

    Social API and UI

---
+++ social-api/routes/posts.ts
@@ -0,0 +1,65 @@
+import { Router } from "express";
+const router = Router();
+
+import { prisma } from "../libs/prisma";
+
+import { auth } from "../middlewares/auth";
+
+// router.get("/",auth, async (req, res) => { checking bearer token for auth
+router.get("/", async (req, res) => {
+		const posts = await prisma.post.findMany({
+			orderBy: {
+				id: "desc",
+			},
+			take: 20,
+			include: {
+				user: true,
+				likes: true,
+				comments: true,
+			},
+		});
+
+		res.json(posts);
+	}
+);
+
+router.post("/", auth, async (req, res) => {
+    const user = res.locals.user;
+    const content = req.body?.content;
+
+    if(!content) {
+        return res.status(400).json({ msg: "content is required" });
+    }
+
+    const post = await prisma.post.create({
+        data: {
+            content,
+            userId: user.id,
+        }
+    });
+
+    res.json(post);
+});
+
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+
+	const post = await prisma.post.findUnique({
+		where: { id: Number(id) },
+		include: {
+			user: true,
+			likes: true,
+			comments: {
+                include: { user: true }
+            },
+		},
+	});
+
+	if (!post) {
+		return res.status(404).json({ msg: "Post not found" });
+	}
+
+	res.json(post);
+});
+
+export default router;
